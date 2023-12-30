f4dfd7b6d8b78bb7ba5441475cf6295e7c84e5fc
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2023-12-30 18:25:28 -0300

    errores utilizando el paquete log

--- src/4-registrodeerrores/main.go
+++ src/4-registrodeerrores/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main(){}
+import (
+	"log"
+	"os"
+)
+
+
+
+func main(){
+	log.SetPrefix("main: ") 
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644) //se crea un archivo de log
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	defer file.Close() //se cierra el archivo de log
+
+
+	log.SetOutput(file) //se le dice a log que escriba en el archivo de log creado
+	log.Print("soy un log")
+
+	// log.SetPrefix("main: ") //se agrega un prefijo a los mensajes -> todos van a empezar con "main:"
+
+
+	// log.Print("Hola mundo 1") //se imprime el tiempo y el mensaje -> 2020/07/21 11:22:33 Hola Mundo
+	// log.Println("Hola mundo 2")
+
+	
+
+	// log.Fatal("Registro de error") //se imprime el tiempo y el mensaje y se detiene la ejecución -> 2020/07/21 11:22:33 Hola Mundo
+	// log.Print("puedes verme?") // esto NO se ejecuta
+
+
+
+}
